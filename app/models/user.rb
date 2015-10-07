97f373f7f251e4ad3b24fa44e61eee73dda78f9e
Winifred Au <winifredau1990@gmail.com>
2015-10-07 13:22:04 -0400

    deleting unnecessary comments in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 require "bcrypt"
-#
+
 class User
   attr_reader :password
   attr_accessor :password_confirmation
@@ -10,10 +10,10 @@ class User
 
   property :id, Serial
   property :email, String, required: true, :unique => true
-#
+
   property :password_digest, Text
   has n, :peeps, through: Resource
-#
+
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
